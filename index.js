f78725ab1049db465bd103c4e05fccf39099f47d
Said Navarrete <msnavarretem@puce.edu.ec>
2025-06-18 23:01:47 -0500

    Created variables

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 let express = require('express');
 let app = express();
 let tareasRoutes = require('./routes/tareas');
+let mongose = require("mongose")
+
+mongose 
+    .connect(process.env.MONGO_URI)
+    .then(()=> console.log("Conectado a MongoDB Atlas"))
+    .catch((err)=> console.error("Error", err));
 
 app.use(express.json());
 app.use('/tareas', tareasRoutes);
